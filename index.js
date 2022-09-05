f788f524b06ca6bf12af88442d010a71eaba8a06
michgur <michael@thegur.net>
2022-09-06 02:03:03 +0300

    added try blocks

--- index.js
+++ index.js
@@ -13,23 +13,31 @@ const twitter = new Client(BEARER_TOKEN);
 
 export async function main(req, res) {
   if (!("query" in req)) {
-    res.status(422).send("missing query")
+    res.status(422).send("missing query");
   } else {
-    const stream = twitter.tweets.tweetsRecentSearch({
-      query: `${req.query} lang:en -is:retweet -is:reply -is:quote is:verified`,
-      "tweet.fields": ["created_at", "public_metrics", "author_id"],
-    })
+    try {
+      const stream = twitter.tweets.tweetsRecentSearch({
+        query: `${req.query} lang:en -is:retweet -is:reply -is:quote is:verified`,
+        "tweet.fields": ["created_at", "public_metrics", "author_id"],
+      })
+    } catch (e) {
+      res.status(500).send(`failed to create tweet stream ${e.toString()}`);
+    }
 
-    for await (const tweets of stream) {
-      console.log(tweets);
-      await bigquery
-        .dataset('tilde_data')
-        .table('tweets')
-        .insert(tweets['data'].map(({ created_at, ...data }) => ({
-          ...data,
-          created_at: created_at ? created_at.slice(0, -1) : undefined
-        })))
+    try {
+      for await (const tweets of stream) {
+        console.log(tweets);
+        await bigquery
+          .dataset('tilde_data')
+          .table('tweets')
+          .insert(tweets['data'].map(({ created_at, ...data }) => ({
+            ...data,
+            created_at: created_at ? created_at.slice(0, -1) : undefined
+          })))
       }
+    } catch (e) {
+      res.status(500).send(`an error occured while processing tweets ${e.toString()}`);
+    }
     res.status(200).send(`successfuly added tweets`);
   }
 }
