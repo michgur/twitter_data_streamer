685a62cd519d308062f551421efa93cd2857c221
michgur <michael@thegur.net>
2022-09-06 00:32:19 +0300

    fixed for-await issues

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Client } from "twitter-api-sdk";
 
 const { BEARER_TOKEN } = process.env;
 
-export function main(req, res) {
+export async function main(req, res) {
   const client = new Client(BEARER_TOKEN);
   const stream = client.tweets.tweetsRecentSearch({
     query: "bananas lang:en -is:retweet -is:reply -is:quote is:verified",
