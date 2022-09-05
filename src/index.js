52a0cb4c32b62473887dd9e128fefb967a9df2f6
michgur <michael@thegur.net>
2022-09-06 00:06:41 +0300

    renamed entry point

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const { BEARER_TOKEN } = process.env;
 
-export function helloWorld(req, res) {
+export function main(req, res) {
   const client = new Client(BEARER_TOKEN);
   const stream = client.tweets.tweetsRecentSearch({
     query: "bananas lang:en -is:retweet -is:reply -is:quote is:verified",
