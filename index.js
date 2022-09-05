cc4d5254c1eb9715d9e11a92b35bb8f1030d5d22
michgur <michael@thegur.net>
2022-09-06 00:56:39 +0300

    added bq insert

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import { Client } from "twitter-api-sdk";
 import { BigQuery } from "@google-cloud/bigquery";
 
-const { BEARER_TOKEN } = process.env;
+const { BEARER_TOKEN, BQ_CREDENTIALS } = process.env;
+
+const twitter = new Client(BEARER_TOKEN);
+
+const bigquery = new BigQuery({
+  credentials: JSON.parse(BQ_CREDENTIALS)
+});
 
 export async function main(req, res) {
-  const client = new Client(BEARER_TOKEN);
-  const stream = client.tweets.tweetsRecentSearch({
+  const stream = twitter.tweets.tweetsRecentSearch({
     query: "bananas lang:en -is:retweet -is:reply -is:quote is:verified",
     "tweet.fields": ["created_at", "public_metrics", "author_id"],
   })
 
   for await (const tweet of stream) {
     console.log(tweet);
+    await bigquery.dataset('tilde_data').table('tweets').insert([tweet])
     res.status(200).send(`Started Successfuly ${tweet}`);
   }
 }
