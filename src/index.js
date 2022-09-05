f5db68c6fd67857d03fa3d6deb07fad5ca30dfff
michgur <michael@thegur.net>
2022-09-05 23:38:40 +0300

    express twitter

--- src/index.js
+++ src/index.js
@@ -7,29 +7,19 @@ const { PORT, BEARER_TOKEN } = process.env;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(`Started Successfuly`);
+app.get('/', async (req, res) => {
+  const client = new Client(BEARER_TOKEN);
+  const stream = client.tweets.tweetsRecentSearch({
+    query: "bananas lang:en -is:retweet -is:reply -is:quote is:verified",
+    "tweet.fields": ["created_at", "public_metrics", "author_id"],
+  })
+
+  for await (const tweet of stream) {
+    console.log(tweet);
+    res.send(`Started Successfuly ${tweet}`);
+  }
 });
 
 app.listen(PORT, () => {
   console.log(`helloworld: listening on port ${PORT}`);
 });
-
-// async function main() {
-//   const client = new Client(BEARER_TOKEN);
-//   const stream = client.tweets.tweetsRecentSearch({
-//     query: "bananas lang:en -is:retweet -is:reply -is:quote is:verified",
-//     "tweet.fields": ["created_at", "public_metrics", "author_id"],
-//   })
-
-//   for await (const tweet of stream) {
-//     console.log(tweet);
-//   }
-// }
-
-// // http.createServer(function (req, res) {
-// //   res.writeHead(200, {'Content-Type': 'text/plain'});
-// //   res.write('Hello World!');
-// //   res.end();
-// // }).listen(PORT);
-// main();
