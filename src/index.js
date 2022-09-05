55fccbcea836443f0491a6736c77b54019605be0
michgur <michael@thegur.net>
2022-09-05 23:31:44 +0300

    express test

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
 import { Client } from "twitter-api-sdk";
 import dotenv from "dotenv";
-import http from "http";
+import express from "express";
 
 dotenv.config();
 const { PORT, BEARER_TOKEN } = process.env;
 
-async function main() {
-  const client = new Client(BEARER_TOKEN);
-  const stream = client.tweets.tweetsRecentSearch({
-    query: "bananas lang:en -is:retweet -is:reply -is:quote is:verified",
-    "tweet.fields": ["created_at", "public_metrics", "author_id"],
-  })
+const app = express();
 
-  for await (const tweet of stream) {
-    console.log(tweet);
-  }
-}
+app.get('/', (req, res) => {
+  res.send(`Started Successfuly`);
+});
 
-// http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.write('Hello World!');
-//   res.end();
-// }).listen(PORT);
-main();
+app.listen(PORT, () => {
+  console.log(`helloworld: listening on port ${PORT}`);
+});
+
+// async function main() {
+//   const client = new Client(BEARER_TOKEN);
+//   const stream = client.tweets.tweetsRecentSearch({
+//     query: "bananas lang:en -is:retweet -is:reply -is:quote is:verified",
+//     "tweet.fields": ["created_at", "public_metrics", "author_id"],
+//   })
+
+//   for await (const tweet of stream) {
+//     console.log(tweet);
+//   }
+// }
+
+// // http.createServer(function (req, res) {
+// //   res.writeHead(200, {'Content-Type': 'text/plain'});
+// //   res.write('Hello World!');
+// //   res.end();
+// // }).listen(PORT);
+// main();
