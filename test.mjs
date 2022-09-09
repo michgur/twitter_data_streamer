dcd9e18d2c4f28457b9abc9434caa38d306a9ef1
michgur <michael@thegur.net>
2022-09-09 13:46:21 +0300

    added server-test

--- test.mjs
+++ test.mjs
@@ -1,18 +1,43 @@
 import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 
-const { main } = await import("./index.js");
+async function testLocal(query) {
+    const { main } = await import("./index.js");
 
-const req = {
-    query: "elon musk"
-};
-const res = {
-    status: (code) => {
-        console.log(`status ${code}`);
-        return res;
-    },
-    send: console.log,
-};
+    const req = {
+        body: { query }
+    };
+    const res = {
+        status: (code) => {
+            console.log(`status ${code}`);
+            return res;
+        },
+        send: console.log,
+    };
 
-main(req, res);
+    main(req, res);
+}
+
+async function testServer(query) {
+    const url = 'https://us-central1-tilde-359707.cloudfunctions.net/twitter-data-dump';
+
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+    const body = { query };
+
+    console.log('sending request');
+    const response = await axios({
+        url,
+        headers,
+        method: 'POST',
+        data: body,
+    });
+
+    console.log(`status ${response.status}`);
+    console.log(response.data);
+}
+
+await testServer('trump')
